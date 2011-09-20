7e2146d0b8a92d89799ccc16f299ca6fe139c593
Lanny Lin <lanny@lannyland.com>
2011-09-20 01:38:49 -0600

    Rewrote count mode algorithm using while loop instead of recursive.

--- IPPA/CountDistModes.cs
+++ IPPA/CountDistModes.cs
@@ -6,6 +6,12 @@ using rtwmatrix;
 
 namespace IPPA
 {
+    class DistPoint
+    {
+        public int row;
+        public int column;
+    }
+
     class CountDistModes
     {
         #region Members
@@ -15,12 +21,13 @@ namespace IPPA
         private RtwMatrix mSmallerMap;
         private RtwMatrix mVisited;
         private RtwMatrix mModes;
-        private List<int[]> lstCurPath;
+        private List<DistPoint> lstCurPath;
         private int Count = 0;
+        private int max_r;
+        private int max_c;
 
         private RtwMatrix mPaths;
         private int pathCount = 0;
-        private int recursiveCount = 0;
 
         #endregion
 
@@ -31,14 +38,20 @@ namespace IPPA
         {
             // First down sample the map
             DownSampleMap();
+            // If uniform distribution, then just return 0 mode
+            if (CheckUniformDist())
+            {
+                return 0;
+            }
             // Create mask to mark visited cells (default 0s)
             mVisited = new RtwMatrix(mSmallerMap.Rows, mSmallerMap.Columns);
             // Create another mask to record mode pixels with connected-component-labeling (default 0s)
             mModes = new RtwMatrix(mSmallerMap.Rows, mSmallerMap.Columns);
             mPaths = new RtwMatrix(mSmallerMap.Rows, mSmallerMap.Columns);
             // Boundaries
-            int max_r = mVisited.Rows - 1;
-            int max_c = mVisited.Columns - 1;
+            max_r = mVisited.Rows - 1;
+            max_c = mVisited.Columns - 1;
+            
             // Loop through all unvisited cells and do local hill climbing
             for (int i = 0; i < mSmallerMap.Rows; i++)
             {
@@ -47,36 +60,55 @@ namespace IPPA
                     // Only do this with unvisited cells
                     if (mVisited[i, j] == 0)
                     {
-                        // Mark self as visited
-                        mVisited[i, j] = 1;
                         // Perform Local Hill Climbing
-                        LocalHillClimbing(i, j, max_r, max_c);
+                        LocalHillClimbing(i, j);
                     }
                 }
             }
 
-            // Debug
-            Console.Write("\n");
-            Console.Write("mModes:\n");
-            for (int i = 0; i < mModes.Rows; i++)
-            {
-                for (int j = 0; j < mModes.Columns; j++)
-                {
-                    Console.Write(mModes[i, j].ToString() + " ");
-                }
-                Console.Write("\n");
-            }
+            //// Debug
+            //Console.Write("\n");
+            //Console.Write("mModes:\n");
+            //for (int i = 0; i < mModes.Rows; i++)
+            //{
+            //    for (int j = 0; j < mModes.Columns; j++)
+            //    {
+            //        Console.Write(mModes[i, j].ToString() + " ");
+            //    }
+            //    Console.Write("\n");
+            //}
 
-            Console.Write("Paths:\n");
-            for (int i = 0; i < mPaths.Rows; i++)
-            {
-                for (int j = 0; j < mPaths.Columns; j++)
-                {
-                    Console.Write(mPaths[i, j].ToString() + " ");
-                }
-                Console.Write("\n");
-            }
+            //Console.Write("Paths:\n");
+            //for (int i = 0; i < mPaths.Rows; i++)
+            //{
+            //    for (int j = 0; j < mPaths.Columns; j++)
+            //    {
+            //        Console.Write(mPaths[i, j].ToString() + " ");
+            //    }
+            //    Console.Write("\n");
+            //}
             return Count;
+        }
+
+        // Checking whether the distribution is a uniform distribution.
+        private bool CheckUniformDist()
+        {
+            bool blnUniform = true;
+            float value = mSmallerMap[0, 0];
+            // Loop through all cells and compare
+            for (int i = 0; i < mSmallerMap.Rows; i++)
+            {
+                for (int j = 0; j < mSmallerMap.Columns; j++)
+                {
+                    if (value != mSmallerMap[i,j])
+                    {
+                        blnUniform = false;
+                        i = mSmallerMap.Rows;
+                        j = mSmallerMap.Columns;
+                    }
+                }
+            }
+            return blnUniform;
         }
 
         // Down sample a given matrix using scale specified in project constants page.
@@ -107,31 +139,23 @@ namespace IPPA
         }
 
         // LHC Algorithm to identify modes
-        private void LocalHillClimbing(int C_i, int C_j, int max_r, int max_c)
+        private void LocalHillClimbing(int C_i, int C_j)
         {
+            // A new path now
             pathCount++;
-            // Recursive function to move up in local hill climbing
-            MoveUp(C_i, C_j, max_r, max_c, C_i, C_j);
-        }
+            
+            #region FindMode
 
-        // Recursive function to always move up until reaching peak
-        private void MoveUp(int C_i, int C_j, int max_r, int max_c, int L_i, int L_j)
-        {
-            mPaths[C_i, C_j] = pathCount;
-            // Did I move up to another cell?
-            Boolean blMovedUp = false;
-
-            // Make sure I don't have too many recursive calls
-            recursiveCount++;
-            if (recursiveCount > 2500)
+            // Climb
+            bool blnMode = false;
+            while (!blnMode)
             {
-                // Pretend I am the peak and stop. This is most likely a uniform distribution.
-                // Restart the counter.
-                recursiveCount = 0;
-            }
-            else
-            {
-                // Visit unvisited neighbors and find a better one (8-connect)
+                // Mark self as visited
+                mVisited[C_i, C_j] = 1;
+                // Label cell with current path count
+                mPaths[C_i, C_j] = pathCount;
+                // Loop through all unvisited neighbors to see if there's someone same or better
+                bool blnMoved = false;
                 for (int i = C_i - 1; i < C_i + 2; i++)
                 {
                     if (i < 0 || i > max_r)
@@ -146,7 +170,7 @@ namespace IPPA
                                 || (i == C_i && j == C_j)        // Don't compare against self
                                 || mVisited[i, j] == 1)          // Don't worry about visited cell
                             {
-                                // No unvisited neighbor in this direction.
+                                // No valid neighbor in this direction.
                             }
                             else
                             {
@@ -157,145 +181,163 @@ namespace IPPA
                                     if (mSmallerMap[C_i, C_j] == mSmallerMap[i, j])
                                     {
                                         // If same, then remember it
-                                        int[] point = new int[2];
-                                        point[0] = C_i;
-                                        point[1] = C_j;
-                                        lstCurPath.Add(point);
+                                        DistPoint p = new DistPoint();
+                                        p.row = C_i;
+                                        p.column = C_j;
+                                        lstCurPath.Add(p);
                                     }
                                     else
                                     {
                                         // If next point is better, clear the list
                                         lstCurPath.Clear();
                                     }
+                                    // Move to this neighbor
+                                    blnMoved = true;
+                                    C_i = i;
+                                    C_j = j;
                                     // Make sure I am done with this round
-                                    int cur_i = i;
-                                    int cur_j = j;
                                     i = C_i + 2;
                                     j = C_j + 2;
-                                    blMovedUp = true;
-                                    // First mark it as visited
-                                    mVisited[cur_i, cur_j] = 1;
-                                    // Move up (or flat)
-                                    MoveUp(cur_i, cur_j, max_r, max_c, C_i, C_j);
                                 }
                             }
                         }
+
                     }
                 }
+                if (!blnMoved)
+                {
+                    // No better neighbor, so I am the mode.
+                    blnMode = true;
+                }
             }
+
+            #endregion
             
-            // Didn't find any neighbor same or better, so I am the peak
-            if (!blMovedUp)
+            #region ValidateMode
+
+            // Mark self as visited
+            mVisited[C_i, C_j] = 1;
+            // Label cell with current path count
+            mPaths[C_i, C_j] = pathCount;
+            // Add self to path (mode section)
+            DistPoint C_p = new DistPoint();
+            C_p.row = C_i;
+            C_p.column = C_j;
+            lstCurPath.Add(C_p);
+
+            //// Debug
+            //Console.Write("\n");
+            //Console.Write("mModes:\n");
+            //for (int i = 0; i < mModes.Rows; i++)
+            //{
+            //    for (int j = 0; j < mModes.Columns; j++)
+            //    {
+            //        Console.Write(mModes[i, j].ToString() + " ");
+            //    }
+            //    Console.Write("\n");
+            //}
+            //Console.Write("Paths:\n");
+            //for (int i = 0; i < mPaths.Rows; i++)
+            //{
+            //    for (int j = 0; j < mPaths.Columns; j++)
+            //    {
+            //        Console.Write(mPaths[i, j].ToString() + " ");
+            //    }
+            //    Console.Write("\n");
+            //}
+
+            // Now let's see if this is really a mode
+            bool blnRealMode = true;
+
+            // Make sure I am really the mode by checking all visited neighbors along the path (mode section)
+            float otherMode = -1;
+            foreach (DistPoint p in lstCurPath)
             {
-                // I am already the best (hopefully)
-                int[] point = new int[2];
-                point[0]=C_i;
-                point[1]=C_j;
-                lstCurPath.Add(point);
-                Count++;
-                bool blnRealMode = true;
-                // Make sure I am really the mode by checking all visited neighbors.
-                float otherMode = -1;
-                foreach (int[] p in lstCurPath)
+                if (!blnRealMode)
                 {
-                    if (!blnRealMode)
+                    // As soon as we find a connected mode, stop the loop.
+                    break;
+                }
+                for (int i = p.row - 1; i < p.row + 2; i++)
+                {
+                    if (i < 0 || i > max_r)
                     {
-                        // As soon as we find a connected mode, stop the loop.
-                        break;
+                        // No neighber in that direction
                     }
-                    for (int i = p[0] - 1; i < p[0] + 2; i++)
+                    else
                     {
-                        if (i < 0 || i > max_r)
+                        for (int j = p.column - 1; j < p.column + 2; j++)
                         {
-                            // No neighber in that direction
-                        }
-                        else
-                        {
-                            for (int j = p[1] - 1; j < p[1] + 2; j++)
+                            DistPoint pp = new DistPoint();
+                            pp.row = i;
+                            pp.column = j;
+                            if (j < 0 || j > max_c                  // No neighber in that direction
+                                || (i == p.row && j == p.column)    // Don't compare against self
+                                || PointInPath(pp)                   // Don't check this path because it doesn't have mode label yet.
+                                || mVisited[i, j] == 0)             // Only check visited nodes
                             {
-                                if (j < 0 || j > max_c          // No neighber in that direction
-                                    || (i == p[0] && j == p[1]) // Don't compare against self
-                                    || PointInPath(i,j)         // Don't check this path because it doesn't have mode label yet.
-                                    || mVisited[i, j] == 0)     // Only check visited nodes
+                                // No valid neighbor
+                            }
+                            else
+                            {
+                                if (mSmallerMap[p.row, p.column] <= mSmallerMap[i, j])
                                 {
-                                }
-                                else
-                                {
-                                    if (mSmallerMap[p[0], p[1]] <= mSmallerMap[i, j])
+                                    // I am not a real mode
+                                    blnRealMode = false;
+                                    if (mSmallerMap[p.row, p.column] == mSmallerMap[i, j]
+                                        && mModes[i, j] >= 0)
                                     {
-                                        // I am not a real mode
-                                        blnRealMode = false;
-                                        if (mSmallerMap[p[0], p[1]] == mSmallerMap[i, j]
-                                            && mModes[i, j] >= 0)
-                                        {
-                                            // I am part of another mode
-                                            otherMode = mModes[i, j];
-                                        }
-                                        // Make sure I am done.
-                                        i = p[0] + 2;
-                                        j = p[1]+ 2;
-                                        // Correct count
-                                        Count--;
+                                        // I am part of another mode
+                                        otherMode = mModes[i, j];
                                     }
+                                    // Make sure I am done.
+                                    i = p.row + 2;
+                                    j = p.column + 2;
                                 }
                             }
                         }
                     }
                 }
-                // Connected-Component-Labeling
-                if (blnRealMode)
-                {
-                    // If I am the real mode, then mark all past cells on path with same height the new modeCount value.
-                    foreach (int[] p in lstCurPath)
-                    {
-                        mModes[p[0], p[1]] = Count;
-                    }
-                }
-                else
-                {
-                    if (otherMode > 0)
-                    {
-                        foreach (int[] p in lstCurPath)
-                        {
-                            mModes[p[0], p[1]] = otherMode;
-                        }
-                    }
-                }
-
-                // Clear list
-                lstCurPath.Clear();
-
-                /*
-                Console.Write("\n");
-                Console.Write("mModes:\n");
-                for (int i = 0; i < mModes.Rows; i++)
-                {
-                    for (int j = 0; j < mModes.Columns; j++)
-                    {
-                        Console.Write(mModes[i, j].ToString() + " ");
-                    }
-                    Console.Write("\n");
-                }
-                Console.Write("Paths:\n");
-                for (int i = 0; i < mPaths.Rows; i++)
-                {
-                    for (int j = 0; j < mPaths.Columns; j++)
-                    {
-                        Console.Write(mPaths[i, j].ToString() + " ");
-                    }
-                    Console.Write("\n");
-                }
-                */
             }
+
+            if (blnRealMode)
+            {
+                Count++;
+            }
+
+            #endregion
+            
+            // Connected-Component-Labeling
+            if (blnRealMode)
+            {
+                // If I am the real mode, then mark all past cells on path with same height the new modeCount value.
+                foreach (DistPoint p in lstCurPath)
+                {
+                    mModes[p.row, p.column] = Count;
+                }
+            }
+            else
+            {
+                if (otherMode > 0)
+                {
+                    foreach (DistPoint p in lstCurPath)
+                    {
+                        mModes[p.row, p.column] = otherMode;
+                    }
+                }
+            }
+
+            // Clear list
+            lstCurPath.Clear();
         }
 
         // See if a point is in the current path mode section
-        private bool PointInPath(int i, int j)
+        private bool PointInPath(DistPoint C_p)
         {
             bool blnInPath = false;
-            foreach (int[] p in lstCurPath)
+            foreach (DistPoint p in lstCurPath)
             {
-                if (p[0] == i && p[1] == j)
+                if (p.row == C_p.row && p.column == C_p.column)
                 {
                     blnInPath = true;
                     break;
@@ -311,7 +353,7 @@ namespace IPPA
         public CountDistModes(ref RtwMatrix reachableregion)
         {
             mReachableRegion = reachableregion;
-            lstCurPath = new List<int[]>();
+            lstCurPath = new List<DistPoint>();
         }
 
         //Destructor
